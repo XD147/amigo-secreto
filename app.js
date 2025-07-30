775278b163f2c4f7b9c73b3f332da11c486d4c4c
GICAMUBE <gicamube@gmail.com>
2025-07-29 22:46:14 -0400

    ActualizaFunciones

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigosLista = [];
-let cntador = 0;
 
 function agregarAmigo() {
     var amigo = document.getElementById("amigo").value;
@@ -28,7 +27,6 @@ function sortearAmigo() {
         alert("No hay amigos en la lista para sortear.");
         return;
     }
-    cntador = amigosLista.length;
     var ganador = amigosLista[Math.floor(Math.random() * amigosLista.length)];
     document.getElementById("resultado").innerHTML = "El amigo secreto es: " + ganador;
     amigosLista = amigosLista.filter(amigo => amigo !== ganador); // Eliminar el ganador de la lista
